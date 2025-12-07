c881c2c573cfc89526a610fb2e75a5068a454a13
Anup Dangi <106950757+AnupDangi@users.noreply.github.com>
2025-12-07 22:44:50 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -67,7 +67,7 @@ app = FastAPI(
 # CORS Setup
 origins = [
     "http://localhost:8080", 
-    # "https://vyaparaishop.vercel.app"
+    "https://vyaparaishop.vercel.app"
     # Allow all for development flexibility
 ]
 
